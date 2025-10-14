c60103f54d2823c02edf067f89af57ff884dea2c
matsmadgaming <matthewdurham96@gmail.com>
2025-10-14 14:19:01 +0100

    I have divided the render function into three sections Input - where player input is handled Logic - where game logic is handled Draw - where the frame is drawn
    I have also implemented a viewport

--- core/src/main/java/io/github/eng1group9/Main.java
+++ core/src/main/java/io/github/eng1group9/Main.java
@@ -4,21 +4,45 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.graphics.Color;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
     private SpriteBatch batch;
     private Texture image;
+    private FitViewport viewport;
+
 
     @Override
     public void create() {
         batch = new SpriteBatch();
+        viewport = new FitViewport(20, 15);
+
         image = new Texture("libgdx.png");
     }
 
     @Override
     public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+        input();
+        logic();
+        draw();
+    }
+
+    public void input() {
+        // Process player inputs here
+
+    }
+
+    public void logic() {
+        // Process game logic here
+
+    }
+
+    public void draw() {
+        // Draw frame here
+        ScreenUtils.clear(Color.BLACK);
+        viewport.apply();
         batch.begin();
         batch.draw(image, 140, 210);
         batch.end();
@@ -28,5 +52,10 @@ public class Main extends ApplicationAdapter {
     public void dispose() {
         batch.dispose();
         image.dispose();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height, true); // true centres the camera
     }
 }
