de919dea47cc68bbb6ad32548de9d6af382de3e1
matsmadgaming <matthewdurham96@gmail.com>
2025-10-14 21:46:09 +0100

    I changed the resolution to 1920x1080, and added the ability to toggle fullscreen with F11.

--- core/src/main/java/io/github/eng1group9/Main.java
+++ core/src/main/java/io/github/eng1group9/Main.java
@@ -1,11 +1,13 @@
 package io.github.eng1group9;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
-import com.badlogic.gdx.graphics.Color;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
@@ -13,11 +15,13 @@ public class Main extends ApplicationAdapter {
     private Texture image;
     private FitViewport viewport;
 
+    private boolean isFullscreen = false;
+
 
     @Override
     public void create() {
         batch = new SpriteBatch();
-        viewport = new FitViewport(20, 15);
+        viewport = new FitViewport(160, 120);
 
         image = new Texture("libgdx.png");
     }
@@ -31,7 +35,16 @@ public class Main extends ApplicationAdapter {
 
     public void input() {
         // Process player inputs here
-
+        if (Gdx.input.isKeyJustPressed(Input.Keys.F11)) {
+            if (isFullscreen) {
+                Gdx.graphics.setWindowedMode(1920, 1080);
+                isFullscreen = false;
+            }
+            else {
+                Gdx.graphics.setFullscreenMode(Gdx.graphics.getDisplayMode());
+                isFullscreen = true;
+            }
+        }
     }
 
     public void logic() {
