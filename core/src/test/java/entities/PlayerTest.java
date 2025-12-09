cac99207248c7dee3c1f2fd709908cde43b804e1
Skylar Garrett (nsq511) <nsq511@york.ac.uk>
2025-12-09 14:11:53 +0000

    test: Added jacoco.

--- core/src/test/java/entities/PlayerTest.java
+++ core/src/test/java/entities/PlayerTest.java
@@ -1,13 +1,32 @@
 package entities;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
+
 import io.github.eng1group9.entities.Player;
+import io.github.eng1group9.entities.Player.*;
 
 public class PlayerTest {
-    @Test
-    void testTest() {
-        assertTrue(true, "This test should not fail.");
-    }
+    // Player p;
+
+    // @BeforeEach
+    // void resetConditions() {
+    //     p = new Player(new Vector2(0, 0), 0);
+    // }
+
+    // @Test
+    // void testTest() {
+    //     assertTrue(true, "This test should not fail.");
+    // }
+
+    // @Test
+    // void hasExitKeyTest() {
+    //     System.out.println(p);
+    //     assertEquals(p.hasExitKey(), false);
+    // }
 }
