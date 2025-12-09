c04cf387019975f0d9d579ef99c5bf6b52a3608d
Skylar Garrett (nsq511) <nsq511@york.ac.uk>
2025-12-09 01:18:59 +0000

    test: Adding a test test to check that github action can run them.

---
+++ core/src/test/java/MainTest.java
@@ -0,0 +1,10 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+    @Test
+    public void testTest() {
+        assertTrue(false, "This test should fail.");
+    }
+}
