10cc66b9b6cd42dcba684f8b437495a288c54c78
kikezos <arikikezos@gmail.com>
2026-01-07 11:10:32 +0000

    Revert "fixed door collisions and books crashing game"
    This reverts commit e8a2040eb58f98e4034090bd2d9c71a883f8e2ee.

--- assets/World/Cashier.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="Cashier" tilewidth="16" tileheight="16" tilecount="2" columns="1">
- <image source="Cashier.png" width="16" height="32"/>
-</tileset>
