fb1d5c43775aaf9de50c0c041f4cab4600444947
Skylar Garrett (nsq511) <nsq511@york.ac.uk>
2026-01-12 01:43:40 +0000

    upload: The jar file cometh.

---
+++ headless/src/test/java/io/github/eng1group9/headless/MainTests.java
@@ -0,0 +1,20 @@
+package io.github.eng1group9.headless;
+import io.github.eng1group9.*;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MainTests extends AbstractHeadlessGdxTest {
+
+
+    @Test
+    void testCreate() {
+        assertTrue(true);
+    }
+
+    @Test
+    void testCheckForLongboi() {
+
+        assertTrue(true);
+    }
+}
